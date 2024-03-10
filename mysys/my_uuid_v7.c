d702277238820d2c682b73a4315df8d7d9205393
StefanoPetrilli <stefanop_1999@hotmail.it>
2024-03-10 16:29:07 +0100

    MDEV-32637 Adds sub millisecond clock precision
    It adds the sub millisecond clock precision following the
    section 6.2 of the UUIDv7 specifications using the method:
    "Replace Left-Most Random Bits with Increased Clock Precision".
    This ensure monotonicity even with high frequency UUIDv7
    generation.

--- mysys/my_uuid_v7.c
+++ mysys/my_uuid_v7.c
@@ -19,14 +19,15 @@
 
   A UUIDv7 has the following structure:
 
-  Field                     Octet #          Note
- unix_ts_ms                 0-5     Big-endian unsigned number of
-                                    Unix epoch timestamp in
-                                    milliseconds.
- version_and_rand_a         6-7     CSPRNG 12 bits multiplexed
-                                    with the version number.
- variant_and_rand_b         8-15    CSPRNG 62 bits multiplexed
-                                    with the version number.
+  Field                       Octet #          Note
+ unix_ts_ms                    0-5     Big-endian unsigned number of
+                                       Unix epoch timestamp in
+                                       milliseconds.
+ version_and_sub_ms_precision  6-7     Sub millisecond clock precision
+                                       encoded to fill all the possible
+                                       values in 12 bits and version.
+ variant_and_rand              8-15    CSPRNG 62 bits multiplexed
+                                       with the version number.
 */
 
 #include "mysys_priv.h"
@@ -35,15 +36,16 @@
 #include <myisampack.h> /* mi_int2store, mi_int4store */
 
 static my_bool my_uuid_v7_inited= 0;
-static uint64 milliseconds;
+static uint64 borrowed_microseconds;
 static uint64 uuid_time= 0;
 
 static mysql_mutex_t LOCK_uuid_v7_generator;
 
-#define UUID_VERSION          0x7000
-#define UUID_VERSION_MASK     0x0FFF
-#define UUID_VARIANT          0x8000000000000000
-#define UUID_VARIANT_MASK     0x3FFFFFFFFFFFFFFF
+#define UUID_VERSION                         0x7000
+#define UUID_VERSION_MASK                    0x0FFF
+#define UUID_VARIANT                         0x8000000000000000
+#define UUID_VARIANT_MASK                    0x3FFFFFFFFFFFFFFF
+#define MICROSECONDS_TO_12BIT_MAPPING_FACTOR 4.096
 
 /**
   Init structures needed for my_uuid_v7
@@ -53,7 +55,7 @@ void my_uuid_v7_init()
   if (my_uuid_v7_inited)
     return;
   my_uuid_v7_inited= 1;
-  milliseconds= 0;
+  borrowed_microseconds= 0;
 
   mysql_mutex_init(key_LOCK_uuid_v7_generator, &LOCK_uuid_v7_generator, MY_MUTEX_INIT_FAST);
 }
@@ -67,14 +69,14 @@ void my_uuid_v7_init()
 void my_uuid_v7(uchar *to)
 {
   uint64 tv, unix_ts_ms;
-  uint16 rand_a, version_and_rand_a;
-  uint64 rand_b, variant_and_rand_b;
+  uint16 sub_ms_precision, version_and_sub_ms_precision;
+  uint64 rand, variant_and_rand;
 
   DBUG_ASSERT(my_uuid_v7_inited);
 
-  tv= my_hrtime().val / 1000;
+  tv= my_hrtime().val;
 
-  // Regulate the access to uuid_time and milliseconds
+  // Regulate the access to uuid_time and microseconds
   mysql_mutex_lock(&LOCK_uuid_v7_generator);
 
   if (likely(tv > uuid_time))
@@ -84,15 +86,15 @@ void my_uuid_v7(uchar *to)
       If we "borrowed time", give it back, just as long as we
       stay ahead of the previous timestamp.
     */
-    if (milliseconds)
+    if (borrowed_microseconds)
     {
       uint64 delta;
       /*
-        -1 so we won't make tv= uuid_time for milliseconds >= (tv - uuid_time)
+        -1 so we won't make tv= uuid_time for microseconds >= (tv - uuid_time)
       */
-      delta= MY_MIN(milliseconds, (uint16)(tv - uuid_time -1));
+      delta= MY_MIN(borrowed_microseconds, (uint16)(tv - uuid_time -1));
       tv-= delta;
-      milliseconds-= delta;
+      borrowed_microseconds-= delta;
     }
   }
   else if (unlikely(tv <= uuid_time))
@@ -101,32 +103,36 @@ void my_uuid_v7(uchar *to)
       If several requests for UUIDs end up on the same tick
       or if the system clock is turned *back*.
       We add milli-seconds to make them different.
-      ( current_timestamp + milliseconds * calls_in_this_period )
+      ( current_timestamp + microseconds * calls_in_this_period )
       may end up > next_timestamp; this is OK. Nonetheless, we'll
-      try to unwind milliseconds when we get a chance to.
+      try to unwind microseconds when we get a chance to.
     */
-    milliseconds += uuid_time - tv + 1;
+    borrowed_microseconds += uuid_time - tv + 1;
     tv = uuid_time + 1;
   }
 
   uuid_time= tv;
   mysql_mutex_unlock(&LOCK_uuid_v7_generator);
 
-  my_random_bytes((unsigned char *)&rand_a, (int) sizeof(rand_a));
-  my_random_bytes((unsigned char *)&rand_b, (int) sizeof(rand_b));
+  my_random_bytes((unsigned char *)&rand, (int) sizeof(rand));
 
-  unix_ts_ms=  (uint64) (tv & 0xFFFFFFFFFFFF);
-
+  unix_ts_ms=  (uint64) ((tv / 1000) & 0xFFFFFFFFFFFF);
+  /**
+   Map all the possible microseconds values (from 0 to 999) to all the
+   values that can be represented in 12 bits (from 0 to 4095) as
+   described in section 6.2, Method 3.
+  */
+  sub_ms_precision= MICROSECONDS_TO_12BIT_MAPPING_FACTOR * (tv % 1000);
   /**
    Clear bits 48-51 and 64-65 to 0 and then set them to the
    value specified by the UUIDv7 specification.
   */
-  version_and_rand_a= (uint16) ((rand_a & UUID_VERSION_MASK) | UUID_VERSION);
-  variant_and_rand_b= (uint64) ((rand_b & UUID_VARIANT_MASK) | UUID_VARIANT);
+  version_and_sub_ms_precision= (uint16) ((sub_ms_precision & UUID_VERSION_MASK) | UUID_VERSION);
+  variant_and_rand= (uint64) ((rand & UUID_VARIANT_MASK) | UUID_VARIANT);
 
   mi_int6store(to, unix_ts_ms);
-  mi_int2store(to+6, version_and_rand_a);
-  mi_int8store(to+8, variant_and_rand_b);
+  mi_int2store(to+6, version_and_sub_ms_precision);
+  mi_int8store(to+8, variant_and_rand);
 }
 
 void my_uuid_v7_end()
